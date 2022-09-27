cd7ede14f3394f82bc84b625f5f2638cf8df4c82
John Zhou <john.ly.zhou@gmail.com>
2022-09-26 17:29:08 -0700

    completed psychometric plotting

---
+++ src/visualize.py
@@ -0,0 +1,51 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+from src.environments import STIMULI
+
+
+def get_block_indices(blocks, block_idx):
+    trial_idx = 0
+    for i in range(block_idx):
+        trial_idx += blocks[i][2]
+    return trial_idx, trial_idx + blocks[block_idx][2]
+
+
+def get_psychometric_percents(actions, rewards, stimuli, num_stimuli, action_type=None, reward_type=None,
+                              stimulus_type=None):
+    if action_type:
+        actions = [None if act != action_type else act for act in actions]
+    if reward_type:
+        rewards = [None if rew != reward_type else rew for rew in rewards]
+    if stimulus_type:
+        stimuli = [None if stim != stimulus_type else stim for stim in stimuli]
+
+    act_rew_stim = list(zip(actions, rewards, stimuli))
+
+    num_left_actions = np.zeros(num_stimuli)
+    num_presentations = np.zeros(num_stimuli)
+
+    for act, rew, stim in act_rew_stim:
+        if act is None or rew is None or stim is None:
+            continue
+        elif act == 0:
+            num_left_actions[stim] += 1
+        num_presentations[stim] += 1
+
+    return num_left_actions / num_presentations
+
+
+def plot_psychometric_curve(block_params, rewards, stimuli, actions):
+    block_percents = []
+    fig = plt.figure()
+    for block_idx in range(len(block_params)):
+        psycho_rewards = rewards[block_idx]
+        psycho_stimuli = stimuli[block_idx]
+        action_idxs = get_block_indices(block_params, block_idx)
+        psycho_actions = actions[action_idxs[0]:action_idxs[1]]
+        percents = get_psychometric_percents(psycho_actions, psycho_rewards, psycho_stimuli, len(STIMULI))
+        block_percents.append(percents)
+        plt.scatter(STIMULI, percents, label=f'{block_params[block_idx][0]}')
+        fig.legend()
+    plt.show()
+    return fig

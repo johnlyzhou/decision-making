8a92648d93d9bfa50821f011d142d7fe3bb70ac5
John Zhou <john.ly.zhou@gmail.com>
2022-09-28 11:23:26 -0700

    add runs for model-based agent

--- run.py
+++ run.py
@@ -1,23 +1,23 @@
-from src.agents import QLearningAgent
-from src.environments import (ProposalTask, run_env, LEFT, RIGHT, ALWAYS_LEFT_STIMULI_IDXS, ALWAYS_RIGHT_STIMULI_IDXS,
-                              SWITCHING_STIMULI_IDXS, STIMULI)
+from src.agents import QLearningAgent, BeliefStateAgent
+from src.switch_2afc_task import Switch2AFCTask, run_experiment
 from src.visualize import plot_psychometric_curve
 
-test_blocks = [(LEFT, 1.0, 100), (RIGHT, 1.0, 100), (LEFT, 1.0, 100), (RIGHT, 1.0, 100)]
-num_stimuli = len(ALWAYS_LEFT_STIMULI_IDXS) + len(ALWAYS_RIGHT_STIMULI_IDXS) + len(SWITCHING_STIMULI_IDXS)
-assert(num_stimuli == len(STIMULI))
+test_blocks = [('LOW', 1.0, 100), ('HIGH', 1.0, 100)]
 
-test_task = ProposalTask(test_blocks)
-num_states = num_stimuli
-num_actions = 2
-learning_rate = 0.1
-epsilon = 0.1
-new_agent = QLearningAgent(num_states, num_actions, learning_rate, epsilon)
+test_task = Switch2AFCTask(test_blocks)
 
-env, agent = run_env(test_task, new_agent, test_blocks)
+learning_rate = 1.0
+epsilon = 0.05
+mf_agent = QLearningAgent(learning_rate, epsilon)
+
+p_rew = 1.0
+p_switch = 0.1
+mb_agent = BeliefStateAgent(p_rew, p_switch)
+print(mb_agent.reward_distribution)
+env, agent = run_experiment(test_task, mb_agent, test_blocks)
 
 actions = agent.action_history
-stimuli = test_task.stimuli_history
+stimuli = test_task.stimulus_history
 rewards = test_task.reward_history
 
 fig = plot_psychometric_curve(test_blocks, rewards, stimuli, actions)

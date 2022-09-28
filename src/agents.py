f2d240680d76db0678bf2f6a1d8606b986435203
John Zhou <john.ly.zhou@gmail.com>
2022-09-28 11:23:01 -0700

    bugfixes for belief state agent, passing tests now

--- src/agents.py
+++ src/agents.py
@@ -1,100 +1,110 @@
 import numpy as np
 
-from src.environments import STIMULI, ACTIONS, LOW_IDX, HIGH_IDX, LEFT, RIGHT, BOUNDARIES
+from src.switch_2afc_task import STIMULI_FREQS, ACTIONS, BOUNDARY_IDX, BOUNDARY_FREQS
 
 
-class Agent:
-    def __init__(self, num_actions):
-        self.num_actions = num_actions
-
-    def sample_action(self, stimulus_idx):
-        pass
-
-    def step(self):
-        pass
-
-    def observe(self):
-        pass
-
-    def update(self, stimulus_idx, action, reward):
-        pass
-
-
-class QLearningAgent(Agent):
-    def __init__(self, num_stimuli, num_actions, learning_rate, epsilon):
-        super().__init__(num_actions)
+class QLearningAgent:
+    """A model-free agent that uses Q-learning to update state-action values in response to received rewards."""
+    def __init__(self, learning_rate, epsilon):
         self.learning_rate = learning_rate
         self.epsilon = epsilon
-        self.stimulus_action_values = [np.ones(num_actions) / num_actions for _ in range(num_stimuli)]
+        # Initialize a uniform probability distribution over actions for each stimulus.
+        self.stimulus_action_values = [np.ones(len(ACTIONS)) / len(ACTIONS) for _ in range(len(STIMULI_FREQS))]
         self.stimulus_action_value_history = []
         self.action_history = []
 
     def sample_action(self, stimulus_idx):
+        """Choose an action according to an epsilon-greedy policy."""
         explore = np.random.random() < self.epsilon
         if explore:
-            action = np.random.randint(0, high=self.num_actions)
+            action = np.random.randint(0, high=len(ACTIONS))
         else:
             action = np.argmax(self.stimulus_action_values[stimulus_idx])
         self.action_history.append(action)
         return action
 
     def update(self, stimulus_idx, action, reward):
+        """Update state-action value based on the observed rewards in this trial."""
         self.stimulus_action_values[stimulus_idx][action] += self.learning_rate * (
                 reward - self.stimulus_action_values[stimulus_idx][action]
         )
         self.stimulus_action_value_history.append(self.stimulus_action_values)
 
 
-class BeliefStateAgent(Agent):
-    def __init__(self, num_actions, p_reward, p_switch):
-        super().__init__(num_actions)
+class BeliefStateAgent:
+    """A model-based agent that tracks the location of the boundary separating left and right stimuli."""
+    def __init__(self, p_reward, p_switch):
+        super().__init__()
+        self.low_boundary_idx = 0
+        self.high_boundary_idx = 1
         self.boundary_beliefs = np.array([0.5, 0.5])
-        self.p_reward = p_reward
-        self.reward_distribution = self.initialize_rew_dist(ACTIONS, STIMULI, BOUNDARIES)
-        self.p_switch = p_switch
+        self.reward_probability = p_reward
+        self.reward_distribution = None
+        self.initialize_reward_distribution()
+        self.switch_probability = p_switch
         self.reward = None
         self.perceived_stimulus = None
         self.choice = None
+        self.action_history = []
+        self.boundary_belief_history = []
 
-    def initialize_rew_dist(self, actions, stimuli, boundaries):
-        num_actions = len(actions)
-        num_stimuli = len(stimuli)
-        num_bounds = len(boundaries)
-        rew_dist = np.ones((num_actions, num_stimuli, num_bounds))
-        for stim_idx in range(num_stimuli):
-            for bound_idx in range(num_bounds):
-                if STIMULI[stim_idx] < BOUNDARIES[bound_idx]:
-                    rew_dist[LEFT, stim_idx, bound_idx] = self.p_reward
-                    rew_dist[RIGHT, stim_idx, bound_idx] = 1 - self.p_reward
+    def initialize_reward_distribution(self):
+        """Initialize the belief distribution over rewards given an action, presented stimulus, and boundary belief."""
+        left = ACTIONS['LEFT']
+        right = ACTIONS['RIGHT']
+        num_actions = len(ACTIONS)
+        num_stimuli = len(STIMULI_FREQS)
+        num_bounds = len(BOUNDARY_FREQS)
+
+        reward_distribution = np.ones((num_actions, num_stimuli, num_bounds))
+        for stimulus_idx in range(num_stimuli):
+            for boundary_idx in range(num_bounds):
+                if STIMULI_FREQS[stimulus_idx] < BOUNDARY_FREQS[boundary_idx]:
+                    reward_distribution[left, stimulus_idx, boundary_idx] = self.reward_probability
+                    reward_distribution[right, stimulus_idx, boundary_idx] = 1 - self.reward_probability
                 else:
-                    rew_dist[LEFT, stim_idx, bound_idx] = 1 - self.p_reward
-                    rew_dist[RIGHT, stim_idx, bound_idx] = self.p_reward
-        return rew_dist
+                    reward_distribution[left, stimulus_idx, boundary_idx] = 1 - self.reward_probability
+                    reward_distribution[right, stimulus_idx, boundary_idx] = self.reward_probability
+        self.reward_distribution = reward_distribution
 
     def sample_action(self, stimulus_idx):
-        stimulus = STIMULI[stimulus_idx]
+        """Sample an action according to a noisy stimulus perception and belief over current boundary location."""
+        stimulus = STIMULI_FREQS[stimulus_idx]
         self.perceived_stimulus = stimulus + np.random.normal(scale=1.0)
         boundary_idx = np.argmax(self.boundary_beliefs)
-        boundary = BOUNDARIES[boundary_idx]
+        boundary = BOUNDARY_FREQS[boundary_idx]
 
         if self.perceived_stimulus < boundary:
-            self.choice = ACTIONS[0]
+            action = ACTIONS['LEFT']
         else:
-            self.choice = ACTIONS[1]
+            action = ACTIONS['RIGHT']
 
-        return self.choice
+        self.action_history.append(action)
+        self.choice = action
+        return action
 
     def update(self, stimulus_idx, action, reward):
-        current_bound_beliefs = [0, 0]
-        for curr_idx in [LOW_IDX, HIGH_IDX]:
-            for prev_idx in [LOW_IDX, HIGH_IDX]:
-                prior = self.boundary_beliefs[prev_idx]
-                rew_prob = self.reward_distribution[action, stimulus_idx, prev_idx]
-                if curr_idx != prev_idx:
-                    trans_probs = self.p_switch
+        """Recursively update beliefs over boundary location based on last trial's outcome."""
+        # Use to index into reward distribution and new boundary beliefs.
+        low_boundary_idx = 0
+        high_boundary_idx = 1
+
+        new_boundary_beliefs = [0, 0]
+
+        for current_boundary_belief in [low_boundary_idx, high_boundary_idx]:
+            for previous_boundary_belief in [low_boundary_idx, high_boundary_idx]:
+                prior = self.boundary_beliefs[previous_boundary_belief]
+                expected_reward_prob = self.reward_distribution[action, stimulus_idx, previous_boundary_belief]
+                if reward:
+                    reward_prob = expected_reward_prob
                 else:
-                    trans_probs = 1 - self.p_switch
-                current_bound_beliefs[curr_idx] += prior * rew_prob * trans_probs
-        current_bound_beliefs /= sum(current_bound_beliefs)
-        self.boundary_beliefs = current_bound_beliefs
-        return current_bound_beliefs
+                    reward_prob = 1 - expected_reward_prob
+
+                if current_boundary_belief != previous_boundary_belief:
+                    trans_probs = self.switch_probability
+                else:
+                    trans_probs = 1 - self.switch_probability
+                new_boundary_beliefs[current_boundary_belief] += prior * reward_prob * trans_probs
+        new_boundary_beliefs /= sum(new_boundary_beliefs)
+        self.boundary_beliefs = new_boundary_beliefs
+        self.boundary_belief_history.append(new_boundary_beliefs)

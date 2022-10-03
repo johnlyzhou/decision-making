a94d9c42326f663618f8632a68f13c2f1a304007
John Zhou <john.ly.zhou@gmail.com>
2022-10-03 11:50:16 -0700

    new method to reformat observations for ssms

--- src/features/build_ssm_features.py
+++ src/features/build_ssm_features.py
@@ -1,3 +1,11 @@
 import numpy as np
 
 
+def build_outputs(*args):
+    """Takes in lists of observations and reshapes them into the correct observation format for the SSM library."""
+    if len(set([len(arg) for arg in args])) != 1:
+        print([len(arg) for arg in args])
+        raise ValueError("All lists of observations should be the same length!")
+
+    obs = list(zip(*args))
+    return np.array(obs)

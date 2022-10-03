40d98ba64c96b42949e56e7d7b0cd082c1fd9438
John Zhou <john.ly.zhou@gmail.com>
2022-10-03 11:47:17 -0700

    add switching agent runs

--- src/data/experiments.py
+++ src/data/experiments.py
@@ -1,8 +1,9 @@
 from omegaconf import OmegaConf
+import numpy as np
 
-from src.data.agents import QLearningAgent, BeliefStateAgent
+from src.data.agents import QLearningAgent, BeliefStateAgent, SwitchingAgent
 from src.data.environments import Block2AFCTask
-from src.visualization.visualize import plot_psychometric_curve
+from src.visualization.psychometric import plot_psychometric_curve
 
 
 class Experiment:
@@ -20,18 +21,34 @@ class Experiment:
             self.epsilon = self.config["epsilon"]
             if not (0 <= self.epsilon <= 1):
                 raise ValueError("Epsilon should be between 0 and 1!")
+
             self.agent = QLearningAgent(self.learning_rate, self.epsilon)
+
         elif self.config["agent"].lower() == "beliefstateagent":
             self.p_reward = self.config["p_reward"]
             self.p_switch = self.config["p_switch"]
             if not (0 <= self.p_reward <= 1 and 0 <= self.p_switch <= 1):
                 raise ValueError("p_reward and p_switch should be between 0 and 1!")
+
             self.agent = BeliefStateAgent(self.p_reward, self.p_switch)
+
+        elif self.config["agent"].lower() == "switchingagent":
+            self.transition_matrix = np.array(self.config["transition_probs"])
+            self.learning_rate = self.config["learning_rate"]
+            self.epsilon = self.config["epsilon"]
+            if not (0 <= self.epsilon <= 1):
+                raise ValueError("Epsilon should be between 0 and 1!")
+            self.p_reward = self.config["p_reward"]
+            self.p_switch = self.config["p_switch"]
+            if not (0 <= self.p_reward <= 1 and 0 <= self.p_switch <= 1):
+                raise ValueError("p_reward and p_switch should be between 0 and 1!")
+
+            agent0 = QLearningAgent(self.learning_rate, self.epsilon)
+            agent1 = self.agent = BeliefStateAgent(self.p_reward, self.p_switch)
+            agents = [agent0, agent1]
+            self.agent = SwitchingAgent(self.transition_matrix, agents)
         else:
             raise NotImplementedError("QLearningAgent and BeliefStateAgent are the only implemented agents!")
-
-    def get_blocks(self):
-        return self.blocks
 
     def run(self):
         """Run an agent through an experiment with parameters set by block_params."""
@@ -45,13 +62,13 @@ class Experiment:
             reward = (agent_action == correct_action)
             self.agent.update(stimuli, agent_action, reward)
 
-    def plot_psychometric(self, save=False, path=None):
+    def plot_psychometric_scatter(self, save=False, path=None):
         if not self.environment.done:
             raise Exception("Run experiment before plotting results!")
 
         blocks = self.blocks
         actions = self.agent.action_history
-        stimuli = self.environment.stimulus_history
+        stimuli = self.environment.stimulus_idx_history
         rewards = self.environment.reward_history
 
         fig = plot_psychometric_curve(blocks, rewards, stimuli, actions)

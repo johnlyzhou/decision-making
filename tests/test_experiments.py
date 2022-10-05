6ad776a72f87e91d308ac302626db0d4161459e0
John Zhou <john.ly.zhou@gmail.com>
2022-10-05 13:01:05 -0700

    changed trial counts test for new history structure

--- tests/test_experiments.py
+++ tests/test_experiments.py
@@ -17,17 +17,12 @@ class TestSwitch2AFCTask(unittest.TestCase):
         self.assertTrue(env.done)
 
     def test_trial_counts(self):
-        blocks = [('LOW', 1.0, 100), ('HIGH', 1.0, 100), ('LOW', 1.0, 100), ('HIGH', 1.0, 100)]
-        env = Block2AFCTask(blocks)
-        for ep in range(sum(block[2] for block in blocks) + 1):
+        env = Block2AFCTask(TEST_BLOCKS)
+        for ep in range(sum(block[2] for block in TEST_BLOCKS) + 1):
             env.step()
             if env.done:
                 break
-        for block_idx in range(len(blocks)):
-            self.assertEqual(len(env.reward_history[block_idx]),
-                             len(env.stimulus_idx_history[block_idx]),
-                             blocks[block_idx][2]
-                             )
+        assert(len(env.reward_history) == len(env.stimulus_history) == len(env.boundary_history))
 
 
 if __name__ == "__main__":

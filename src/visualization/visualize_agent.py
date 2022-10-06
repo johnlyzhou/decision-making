12bdbac032dab18b05139fd7a74d0c3ce02034cb
John Zhou <john.ly.zhou@gmail.com>
2022-10-05 22:17:17 -0700

    plot action-values of q learner over time

---
+++ src/visualization/visualize_agent.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+
+from src.data.environments import STIMULI_FREQS
+from src.utils import get_block_indices
+
+
+def plot_action_values(stimulus_action_value_history, blocks):
+    """
+    Plot left action-values of Q-learning agent over time, normalized to [0, 1] (so right action-value is 1 - left
+    action-value).
+    :param blocks: block parameters for experiment.
+    :param stimulus_action_value_history: a list of lists of numpy arrays, where each numpy array is the action-values
+    for a particular stimulus and each inner list is the action-values for all stimuli at that timestep.
+    :return:
+    """
+    block_idxs = get_block_indices(blocks)
+    fig = plt.figure()
+    for i in block_idxs:
+        plt.axvline(x=i[1], color="black")
+    for stimulus_idx in range(len(STIMULI_FREQS)):
+        left_action_values = [trial[stimulus_idx][0] for trial in stimulus_action_value_history]
+        plt.plot(left_action_values, label=f'Stimulus: {stimulus_idx}')
+        fig.legend()
+    plt.show()

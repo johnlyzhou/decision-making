e08a3bb7be2523bf86e85bc91c51216ad896671b
John Zhou <john.ly.zhou@gmail.com>
2022-10-05 17:06:48 -0700

    added docstrings

--- src/models/fit_sigmoidal.py
+++ src/models/fit_sigmoidal.py
@@ -3,5 +3,13 @@ import math
 from scipy.optimize import minimize
 
 
-def sigmoidal(t, eps, alpha, s):
-    return eps + (1 - 2 * eps) / (1 + math.exp(-alpha(t - s)))
+def epsilon_logistic(x, eps, k, x_0):
+    """
+    Logistic curve with additional epsilon "lapse" parameter.
+    :param eps: float in range [0, 1], epsilon exploration parameter, influences maximum value.
+    :param k: logistic growth rate indicating steepness of the curve.
+    :param x: int indicating trial index, where the first trial after the block switch has index 0.
+    :param x_0: trial index at curve's midpoint.
+    :return: float in range [0, 1] indicating some percentage.
+    """
+    return eps + (1 - 2 * eps) / (1 + math.exp(-k(x - x_0)))

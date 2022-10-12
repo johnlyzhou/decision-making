dbabeb084b2e40845944b6dd2d611c4022066b1f
John Zhou <john.ly.zhou@gmail.com>
2022-10-11 22:55:01 -0700

    added env interface, new block task, and helper functions, updated constants to match real experiments

--- src/data/environments.py
+++ src/data/environments.py
@@ -1,53 +1,148 @@
+import abc
 import random
 
 import numpy as np
+from numpy import ndarray
+
+"""Which side is the rewarded action for the Le et al. 2022 blockHMM paper."""
+SIDE = {'LEFT': 0, 'RIGHT': 1}
 
 """Frequency boundaries separate stimuli that reward left choices from stimuli that reward right choices."""
 BOUNDARY_IDX = {'LOW': 0, 'HIGH': 1}
 """Indices in BOUNDARY_IDX map to actual frequency values of the boundaries in BOUNDARY_FREQS"""
-BOUNDARY_FREQS = [8, 14]
+BOUNDARY_FREQS = [8, 16]
 
 """Dictionary keys represent the choice rewarded from the presentation of that stimulus (index)."""
 STIMULI_IDXS = {'LEFT': [0], 'SWITCH': [1, 2], 'RIGHT': [3]}
 """The indices in STIMULI_IDXS map to actual frequency values of the stimuli in STIMULI_FREQS."""
-STIMULI_FREQS = [6, 10, 12, 16]
+STIMULI_FREQS = [3, 10, 14, 21]
 
 """Actions are choices the mouse can take. In a 2AFC task, it only has 2 choices: left or right)."""
 ACTIONS = {'LEFT': 0, 'RIGHT': 1}
 
 
-def validate_blocks(blocks):
+class EnvironmentInterface(metaclass=abc.ABCMeta):
     """
-    Ensure block parameters are in the correct format.
-    :param blocks: list of tuples, where each tuple represents a block with format (boundary, reward_probability,
-    num_trials).
-    :return: True if block parameters are in the correct format.
+    An abstract interface specifying the blueprint for task environment objects.
     """
-    if not all([len(block) == 3 for block in blocks]):
-        raise ValueError("Block parameters should be formatted in a list of tuples (boundary, reward_probability, "
-                         "num_trials).")
-    for block in blocks:
-        boundary, reward_prob, num_trials = block
-        if boundary not in BOUNDARY_IDX:
-            raise ValueError(f"Boundaries should match one of {BOUNDARY_IDX.keys()}!")
-        if reward_prob < 0 or reward_prob > 1.0:
-            raise ValueError("Reward probability should be between 0 and 1!")
-        if type(num_trials) != int or num_trials < 0:
-            raise ValueError("Reward probability should be a positive integer!")
-    return True
+    def __init__(self, blocks) -> None:
+        self.done = False
+        self.blocks = blocks
+
+    @classmethod
+    def __subclasshook__(cls, subclass):
+        return (hasattr(subclass, 'sample_action') and
+                callable(subclass.sample_action) and
+                hasattr(subclass, 'update') and
+                callable(subclass.update) or
+                NotImplemented)
+
+    def end(self) -> None:
+        """Set ending flag for environment after all trials in all blocks have finished."""
+        self.done = True
+
+    def get_num_total_trials(self) -> int:
+        """Return total number of trials across all blocks."""
+        return sum(block[2] for block in self.blocks)
+
+    @abc.abstractmethod
+    def step(self) -> None:
+        """Step through one trial of the experiment."""
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def reset(self) -> None:
+        """Reset environment to initial state."""
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def sample_schedule(self) -> list:
+        raise NotImplementedError
 
 
-class Block2AFCTask:
+class BlockTask(EnvironmentInterface):
+    """2AFC decision-making task with switching action rewards from Le et al. 2022."""
+
+    def __init__(self, blocks: list[tuple[str, float, int]]) -> None:
+        """
+        :param blocks: list of tuples, where each tuple represents a block with format (boundary, reward_probability,
+        num_trials).
+        """
+        super().__init__(blocks)
+        validate_blocks(self.blocks, "BlockTask")
+
+        self.current_block_idx = 0
+        self.current_trial_idx = -1
+        self.block_reward_schedule = None
+        self.reward_history = []
+
+    def get_current_rewarded_action(self) -> int:
+        """Return correct choice to receive a reward (not necessarily the one received by the agent)."""
+        return self.block_reward_schedule[self.current_trial_idx]
+
+    def step(self) -> None:
+        """Step through one trial of the experiment."""
+        num_trials_in_block = self.blocks[self.current_block_idx][2]
+
+        # Check if environment is finished.
+        if self.done is True:
+            print("Environment has finished running! Check value of ProposalTaskObject.done to exit any loops.")
+            return
+
+        # Check if we've finished the current block.
+        if self.current_trial_idx >= num_trials_in_block - 1:
+            self.current_trial_idx = -1
+            self.current_block_idx += 1
+
+        # Check if we've finished the last block.
+        if self.current_block_idx >= len(self.blocks):
+            self.end()
+            return
+
+        # Check if we're starting a new block: if so, generate new trial schedule.
+        if self.current_trial_idx == -1:
+            self.block_reward_schedule = self.sample_schedule()
+            self.reward_history += list(self.block_reward_schedule)
+
+        self.current_trial_idx += 1
+
+    def reset(self) -> None:
+        """Reset environment to initial state."""
+        self.current_block_idx = 0
+        self.current_trial_idx = -1
+        self.block_reward_schedule = None
+        self.reward_history = []
+
+        self.done = False
+
+    def sample_schedule(self) -> list:
+        """
+        Generate a schedule of trials for the current block, including the stimuli to be presented and the
+        corresponding correct choices required to receive a reward.
+        :return: two lists of ints of length equal to the number of trials in the block, the first containing indices of
+        stimuli presented, the second containing which action will be rewarded, in {0, 1} with 0: left and 1: right.
+        """
+        side, pr_reward, num_trials = self.blocks[self.current_block_idx]
+        if side == "LEFT":
+            weights = [pr_reward, 1 - pr_reward]
+        else:
+            weights = [1 - pr_reward, pr_reward]
+        block_reward_schedule = random.choices(list(ACTIONS.values()), weights=weights, k=num_trials)
+
+        return block_reward_schedule
+
+
+class BlockStimulusTask(EnvironmentInterface):
     """2AFC perceptual decision-making task with switching category boundaries from Liu, Xin, and Xu 2021."""
-    def __init__(self, blocks, balance_mode="reward"):
+
+    def __init__(self, blocks: list[tuple[str, float, int]], balance_mode: str = "reward") -> None:
         """
         :param blocks: list of tuples, where each tuple represents a block with format (boundary, reward_probability,
         num_trials).
         :param balance_mode: string indicating how probabilities of stimulus appearances should be balanced.
         """
-        validate_blocks(blocks)
-        self.blocks = blocks
-        self.total_trials = sum(block[2] for block in self.blocks)
+        super().__init__(blocks)
+        validate_blocks(self.blocks, "BlockStimulusTask")
         self.balance_mode = balance_mode
 
         self.current_block_idx = 0
@@ -65,15 +160,15 @@ class Block2AFCTask:
 
         self.done = False
 
-    def get_current_stimulus(self):
+    def get_current_stimulus(self) -> int:
         """Return stimulus presented during current trial."""
         return self.block_stimulus_schedule[self.current_trial_idx]
 
-    def get_current_rewarded_action(self):
+    def get_current_rewarded_action(self) -> int:
         """Return correct choice to receive a reward (not necessarily the one received by the agent)."""
         return self.block_reward_schedule[self.current_trial_idx]
 
-    def step(self):
+    def step(self) -> None:
         """Step through one trial of the experiment."""
         num_trials_in_block = self.blocks[self.current_block_idx][2]
 
@@ -101,11 +196,7 @@ class Block2AFCTask:
 
         self.current_trial_idx += 1
 
-    def end(self):
-        """Set ending flag for environment after all trials in all blocks have finished."""
-        self.done = True
-
-    def reset(self):
+    def reset(self) -> None:
         """Reset environment to initial state."""
         self.current_block_idx = 0
         self.block_stimulus_schedule = None
@@ -115,7 +206,7 @@ class Block2AFCTask:
         self.reward_history = []
         self.done = False
 
-    def sample_schedule(self):
+    def sample_schedule(self) -> tuple[list[int], ndarray]:
         """
         Generate a schedule of trials for the current block, including the stimuli to be presented and the
         corresponding correct choices required to receive a reward.
@@ -124,14 +215,7 @@ class Block2AFCTask:
         """
         boundary, pr_reward, num_trials = self.blocks[self.current_block_idx]
 
-        left_stimuli_idxs = STIMULI_IDXS['LEFT'].copy()
-        right_stimuli_idxs = STIMULI_IDXS['RIGHT'].copy()
-
-        if boundary == 'HIGH':
-            left_stimuli_idxs += STIMULI_IDXS['SWITCH']
-        elif boundary == 'LOW':
-            right_stimuli_idxs = STIMULI_IDXS['SWITCH'] + right_stimuli_idxs
-
+        left_stimuli_idxs, right_stimuli_idxs = get_stimuli_by_side(boundary)
         num_left_stimuli = len(left_stimuli_idxs)
         num_right_stimuli = len(right_stimuli_idxs)
 
@@ -165,3 +249,45 @@ class Block2AFCTask:
                     reward_schedule[trial_idx] = 1 - label_schedule[trial_idx]
 
         return stimuli_schedule, reward_schedule
+
+
+def validate_blocks(blocks: list[tuple[str, float, int]], task_type: str) -> bool:
+    """
+    Ensure block parameters are in the correct format.
+    :param task_type: string indicating for which task to validate blocks.
+    :param blocks: list of tuples, where each tuple represents a block with format (boundary, reward_probability,
+    num_trials).
+    :return: True if block parameters are in the correct format.
+    """
+    if not all([len(block) == 3 for block in blocks]):
+        raise ValueError("Block parameters should be formatted in a list of tuples (boundary, reward_probability, "
+                         "num_trials).")
+
+    for block in blocks:
+        if task_type == "BlockStimulusTask":
+            boundary, reward_prob, num_trials = block
+            if boundary not in BOUNDARY_IDX:
+                raise ValueError(f"Boundaries should match one of {BOUNDARY_IDX.keys()}!")
+        elif task_type == "BlockTask":
+            side, reward_prob, num_trials = block
+            if side not in SIDE:
+                raise ValueError(f"Sides should match one of {SIDE.keys()}!")
+        else:
+            raise NotImplementedError("Task not implemented! Choose one of \'BlockTask\' or \'BlockStimulusTask\'.")
+        if reward_prob < 0 or reward_prob > 1.0:
+            raise ValueError("Reward probability should be between 0 and 1!")
+        if type(num_trials) != int or num_trials < 0:
+            raise ValueError("Reward probability should be a positive integer!")
+    return True
+
+
+def get_stimuli_by_side(boundary: str) -> tuple[list[int], list[int]]:
+    left_stimuli_idxs = STIMULI_IDXS['LEFT'].copy()
+    right_stimuli_idxs = STIMULI_IDXS['RIGHT'].copy()
+
+    if boundary == 'HIGH':
+        left_stimuli_idxs += STIMULI_IDXS['SWITCH']
+    elif boundary == 'LOW':
+        right_stimuli_idxs = STIMULI_IDXS['SWITCH'] + right_stimuli_idxs
+
+    return left_stimuli_idxs, right_stimuli_idxs

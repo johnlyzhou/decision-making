34224cced6e69dd6263eb0ec0f5e2baf38713403
John Zhou <john.ly.zhou@gmail.com>
2022-10-18 00:57:26 -0700

    created interface for real and synth expt to subclass

--- src/data/experiments.py
+++ src/data/experiments.py
@@ -1,92 +1,80 @@
-from typing import Union
+import abc
+from typing import Union, Any, Tuple, Type
 
 from omegaconf import OmegaConf
 from omegaconf.dictconfig import DictConfig
 import numpy as np
 
-from src.data.agents import QLearningAgent, BeliefStateAgent, SwitchingAgent, BlockSwitchingAgent, InferenceAgent
-from src.data.environments import BlockStimulusTask, BlockTask
+from src.data.agents import QLearningAgent, BeliefStateAgent, SwitchingAgent, BlockSwitchingAgent, InferenceAgent, \
+    UnknownAgent
+from src.data.environments import SwitchingStimulusTask, DynamicForagingTask, EnvironmentInterface
+from src.data.real_data import DynamicForagingData, convert_real_blocks, convert_real_actions
 
 
-class Experiment:
+class ExperimentInterface(metaclass=abc.ABCMeta):
+    def __init__(self):
+        self.__done = False
+
+    @property
+    def done(self):
+        return self.__done
+
+    @classmethod
+    def __subclasshook__(cls, subclass):
+        return (hasattr(subclass, 'run') and
+                callable(subclass.run) or
+                NotImplemented)
+
+    @abc.abstractmethod
+    def run(self, **kwargs) -> int:
+        raise NotImplementedError
+
+
+class RealExperiment(ExperimentInterface):
+    def __init__(self, filename: str = None, task_type: Type[EnvironmentInterface] = None):
+        super().__init__()
+        self.__done = True
+        self.task_type = task_type
+        if self.task_type is DynamicForagingTask:
+            self.data = DynamicForagingData(filename)
+            self.blocks = convert_real_blocks(self.data.block, self.data.correct_side)
+            self.action_history = convert_real_actions(self.data.response_side)
+            self.reward_history = self.data.rewarded
+            self.environment = DynamicForagingTask(self.blocks, self.reward_history)
+            self.agent = UnknownAgent(self.action_history)
+        else:
+            raise NotImplementedError
+
+    def run(self):
+        raise NotImplementedError
+
+
+class SynthExperiment(ExperimentInterface):
     """
-    Sets up a task environment and agent(s) and runs an experiment based on the settings in a configuration file.
+    Sets up a task environment and agent(s) and runs a synthetic experiment based on configuration settings.
     """
-    def __init__(self, config: Union[str, DictConfig]) -> None:
+
+    def __init__(self, config: Union[str, DictConfig] = None) -> None:
         """
-        :param config: string containing path to config file.
+        :param config: string containing path to config file or a dictionary config.
         """
-        self.done = False
+        super().__init__()
         if type(config) == str:
             self.config = OmegaConf.load(config)
         else:
             self.config = config
-        self.task_type = None
 
-        if self.config["environment"].lower() == "blockstimulustask":
-            self.task_type = BlockStimulusTask
-            self.blocks = self.config["blocks"]
-            self.environment = BlockStimulusTask(self.blocks)
-        elif self.config["environment"].lower() == "blocktask":
-            self.task_type = BlockTask
-            self.blocks = self.config["blocks"]
-            self.environment = BlockTask(self.blocks)
-        else:
-            raise NotImplementedError
-
-        if self.config["agent"].lower() == "qlearningagent":
-            self.learning_rate = self.config["learning_rate"]
-            self.epsilon = self.config["epsilon"]
-            if not (0 <= self.epsilon <= 1):
-                raise ValueError("Epsilon should be between 0 and 1!")
-
-            self.agent = QLearningAgent(self.learning_rate, self.epsilon, task=self.task_type)
-
-        elif self.config["agent"].lower() == "inferenceagent":
-            self.p_reward = self.config["p_reward"]
-            self.p_switch = self.config["p_switch"]
-            if not (0 <= self.p_reward <= 1 and 0 <= self.p_switch <= 1):
-                raise ValueError("p_reward and p_switch should be between 0 and 1!")
-
-            self.agent = InferenceAgent(self.p_reward, self.p_switch, task=self.task_type)
-
-        elif self.config["agent"].lower() == "beliefstateagent":
-            self.p_reward = self.config["p_reward"]
-            self.p_switch = self.config["p_switch"]
-            if not (0 <= self.p_reward <= 1 and 0 <= self.p_switch <= 1):
-                raise ValueError("p_reward and p_switch should be between 0 and 1!")
-
-            self.agent = BeliefStateAgent(self.p_reward, self.p_switch, task=self.task_type)
-
-        elif self.config["agent"].lower() == "switchingagent" or self.config["agent"].lower() == "blockswitchingagent":
-            self.transition_matrix = np.array(self.config["transition_probs"])
-            self.learning_rate = self.config["learning_rate"]
-            self.epsilon = self.config["epsilon"]
-            if not (0 <= self.epsilon <= 1):
-                raise ValueError("Epsilon should be between 0 and 1!")
-            self.p_reward = self.config["p_reward"]
-            self.p_switch = self.config["p_switch"]
-            if not (0 <= self.p_reward <= 1 and 0 <= self.p_switch <= 1):
-                raise ValueError("p_reward and p_switch should be between 0 and 1!")
-
-            agent0 = QLearningAgent(self.learning_rate, self.epsilon, task=self.task_type)
-            agent1 = self.agent = BeliefStateAgent(self.p_reward, self.p_switch, task=self.task_type)
-            agents = [agent0, agent1]
-            if self.config["agent"].lower() == "switchingagent":
-                self.agent = SwitchingAgent(self.transition_matrix, agents, task=self.task_type)
-            elif self.config["agent"].lower() == "blockswitchingagent":
-                self.agent = BlockSwitchingAgent(self.transition_matrix, agents, task=self.task_type)
-        else:
-            raise NotImplementedError
+        self.task_type, self.blocks, self.environment = self.__init_environment()
+        self.agent = self.__init_agent()
 
     def run(self) -> None:
-        """Run an agent through an experiment with parameters set by block_params."""
+        """Run an agent through an environment."""
         for ep in range(self.environment.get_num_total_trials() + 1):
             self.environment.step()
             if self.environment.done:
-                self.done = True
+                self.__done = True
                 break
-            if self.task_type == BlockStimulusTask:
+            if self.task_type == SwitchingStimulusTask:
                 stimuli = self.environment.get_current_stimulus()
                 agent_action = self.agent.sample_action(stimuli)
                 correct_action = self.environment.get_current_rewarded_action()
@@ -95,10 +83,56 @@ class Experiment:
                     self.agent.update(agent_action, reward, stimuli, block_switch=True)
                 else:
                     self.agent.update(agent_action, reward, stimuli)
-            elif self.task_type == BlockTask:
+            elif self.task_type == DynamicForagingTask:
                 agent_action = self.agent.sample_action()
                 correct_action = self.environment.get_current_rewarded_action()
                 reward = (agent_action == correct_action)
                 self.agent.update(agent_action, reward)
             else:
                 raise NotImplementedError
+
+    def __init_environment(self) -> Tuple[Any, Tuple, Any]:
+        if self.config["environment"].lower() == "switchingstimulustask":
+            task_type = SwitchingStimulusTask
+            blocks = self.config["blocks"]
+            environment = SwitchingStimulusTask(blocks)
+        elif self.config["environment"].lower() == "dynamicforagingtask":
+            task_type = DynamicForagingTask
+            blocks = self.config["blocks"]
+            environment = DynamicForagingTask(blocks)
+        else:
+            raise NotImplementedError
+
+        return task_type, blocks, environment
+
+    def __init_agent(self) -> Any:
+        if self.config["agent"].lower() == "qlearningagent":
+            learning_rate = self.config["learning_rate"]
+            epsilon = self.config["epsilon"]
+            agent = QLearningAgent(learning_rate, epsilon, task=self.task_type)
+        elif self.config["agent"].lower() == "inferenceagent":
+            p_reward = self.config["p_reward"]
+            p_switch = self.config["p_switch"]
+            agent = InferenceAgent(p_reward, p_switch)
+        elif self.config["agent"].lower() == "beliefstateagent":
+            p_reward = self.config["p_reward"]
+            p_switch = self.config["p_switch"]
+            agent = BeliefStateAgent(p_reward, p_switch)
+        elif self.config["agent"].lower() == "switchingagent" or self.config["agent"].lower() == "blockswitchingagent":
+            transition_matrix = np.array(self.config["transition_probs"])
+            learning_rate = self.config["learning_rate"]
+            epsilon = self.config["epsilon"]
+            p_reward = self.config["p_reward"]
+            p_switch = self.config["p_switch"]
+            agents = [QLearningAgent(learning_rate, epsilon, task=self.task_type),
+                      BeliefStateAgent(p_reward, p_switch)]
+            if self.config["agent"].lower() == "switchingagent":
+                agent = SwitchingAgent(transition_matrix, agents)
+            elif self.config["agent"].lower() == "blockswitchingagent":
+                agent = BlockSwitchingAgent(transition_matrix, agents)
+            else:
+                raise NotImplementedError
+        else:
+            raise NotImplementedError
+
+        return agent

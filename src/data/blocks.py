5ef1b0219498051f3e97d12d18f484e4111abd03
John Zhou <john.ly.zhou@gmail.com>
2022-10-18 10:41:54 -0700

    pytorch dataloaders for block data

---
+++ src/data/blocks.py
@@ -0,0 +1,46 @@
+from typing import List, Tuple, Any
+
+import numpy as np
+import torch
+from torch import Tensor
+from torch.utils.data import Dataset
+
+from src.data.real_data import DynamicForagingData, normalize_block_side, convert_real_blocks, \
+    convert_real_actions
+from src.utils import blockify
+
+
+class RealDataset(Dataset):
+    def __init__(self, data_file: str, min_len: int = 15) -> None:
+        self.data_file = data_file
+        self.min_len = min_len
+        self.data = self.__ingest_real_data()
+
+    def __getitem__(self, idx: int) -> Tensor:
+        return self.data[idx]
+
+    def __len__(self) -> int:
+        return len(self.data)
+
+    def __ingest_real_data(self) -> list[Tensor]:
+        real_expt = DynamicForagingData(self.data_file)
+        blocks = convert_real_blocks(real_expt.block, real_expt.correct_side)
+        actions = convert_real_actions(real_expt.response_side)
+        blocked_actions = blockify(blocks, actions)
+        normalized_actions = [torch.tensor(normalize_block_side(blocked_actions[block_idx], blocks[block_idx][0]
+                                                                )[: self.min_len])
+                              for block_idx in range(len(blocks)) if blocks[block_idx][2] >= self.min_len]
+        return normalized_actions
+
+
+class SynthDataset(Dataset):
+    def __init__(self, data_file: str, label_file: str, min_len: int = 15) -> None:
+        self.min_len = min_len
+        self.data = [torch.tensor(sample) for sample in list(np.load(data_file).T)]
+        self.labels = list(np.load(label_file).T)
+
+    def __getitem__(self, idx: int) -> tuple[Tensor, Any]:
+        return self.data[idx], self.labels[idx]
+
+    def __len__(self) -> int:
+        return len(self.data)

18e06bff6b9c307a0c3b906059e67f2925a801f0
John Zhou <john.ly.zhou@gmail.com>
2022-10-25 20:31:24 -0700

    add sigmoid loss for learning sigmoid params directly from trial data

--- src/models/losses.py
+++ src/models/losses.py
@@ -1,5 +1,5 @@
 import torch
-from torch import tensor
+from torch import tensor, nn
 
 
 def gaussian_nll(x: tensor, x_hat: tensor, scale: int = 1):
@@ -10,3 +10,10 @@ def gaussian_nll(x: tensor, x_hat: tensor, scale: int = 1):
 
 def kl_divergence(mu: tensor, log_var: tensor):
     return torch.mean(0.5 * torch.sum(log_var.exp() - log_var + mu ** 2 - 1, dim=1), dim=0)
+
+
+def sigmoid_loss(x: tensor, params: tensor):
+    loss = nn.MSELoss()
+    x_range = torch.arange(0, x.size(dim=2))
+    x_hat = params[:, :, 0] + (1 - 2 * params[:, :, 0]) / (1 + torch.exp(-params[:, :, 1] * (x_range - params[:, :, 2])))
+    return loss(x, torch.unsqueeze(x_hat, 1))

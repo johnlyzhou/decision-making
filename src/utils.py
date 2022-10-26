5167cbf2987741ff38a47407e8717ea51d40252b
John Zhou <john.ly.zhou@gmail.com>
2022-10-25 20:32:03 -0700

    add multiple methods for converting block data for analysis

--- src/utils.py
+++ src/utils.py
@@ -63,3 +63,90 @@ def validate_transition_matrix(transition_matrix: ndarray) -> None:
     for row in range(transition_matrix.shape[0]):
         if np.sum(transition_matrix[row]) != 1:
             raise ValueError("Rows of transition probabilities should sum to 1!")
+
+
+def normalize_block_side(action_block: List[int], side: str, wrong_val: int = 0) -> List[int]:
+    """Switch block to make choice matching the hidden state of the trial 1 and the alternative -1."""
+    if wrong_val == 0:
+        if side == "LEFT":
+            return [int(action == 0) for action in action_block]
+        else:
+            return [int(action == 1) for action in action_block]
+    else:
+        if side == "LEFT":
+            return [1 if action == 0 else wrong_val for action in action_block]
+        else:
+            return [1 if action == 1 else wrong_val for action in action_block]
+
+
+def pad_ragged_blocks(normalized_blocks: List[List[int]], max_len: int = 45) -> ndarray:
+    """Take the average choice across blocks of trials with varying lengths."""
+    if not max_len:
+        max_len = max([len(block) for block in normalized_blocks])
+    padded_blocks = np.zeros((max_len, len(normalized_blocks)))
+    for idx, block in enumerate(normalized_blocks):
+        block = np.array(block)
+        lengthened_block = np.pad(block, pad_width=(0, max_len - block.size), mode='constant', constant_values=0)
+        padded_blocks[:, idx] = lengthened_block
+    return padded_blocks
+
+
+def truncate_blocks(blocks: List[List[int]], truncate_length: int = 15) -> List[List[int]]:
+    truncated_blocks = []
+    for block in blocks:
+        truncated_blocks.append(block[:truncate_length])
+    return truncated_blocks
+
+
+def average_blocks(normalized_blocks: List[List[int]], max_len: int = 45, mode: str = 'ragged') -> List[float]:
+    """Take the average choice across blocks of trials with varying lengths."""
+    if not max_len:
+        max_len = max([len(block) for block in normalized_blocks])
+    if mode == 'ragged':
+        ragged_sum = np.zeros(max_len)
+        idx_count = np.zeros(max_len)
+        for block in normalized_blocks:
+            block = np.array(block)
+            block_idxs = np.ones(block.size)
+            block_idxs = np.pad(block_idxs, pad_width=(0, max_len - block.size), mode='constant', constant_values=0)
+            idx_count += block_idxs
+            lengthened_block = np.pad(block, pad_width=(0, max_len - block.size), mode='constant', constant_values=0)
+            ragged_sum += lengthened_block
+        return ragged_sum / idx_count
+    else:
+        min_len = min([len(block) for block in normalized_blocks])
+        uniform_sum = np.zeros(min_len)
+        for block in normalized_blocks:
+            block = np.array(block)
+            truncated_block = block[:min_len]
+            uniform_sum += truncated_block
+        return uniform_sum / len(normalized_blocks)
+
+
+def convert_real_blocks(real_blocks: List[int], real_correct_side: List[int]) -> List[Tuple]:
+    real_blocks = np.array(real_blocks)
+    unique, counts = np.unique(real_blocks, return_counts=True)
+    blocks = []
+    if real_correct_side[0] == 1:
+        side = "LEFT"
+    else:
+        side = "RIGHT"
+    for num_trials in counts:
+        blocks.append((side, 1.0, int(num_trials)))
+        if side == "LEFT":
+            side = "RIGHT"
+        else:
+            side = "LEFT"
+    return blocks
+
+
+def convert_real_actions(real_actions: List[int]) -> List[int]:
+    for i, act in enumerate(real_actions):
+        if np.isnan(act):
+            real_actions[i] = 1
+    return [action - 1 for action in real_actions]
+
+
+def normalize(feats):
+    """Normalize features for clustering/classification"""
+    return (feats - np.expand_dims(np.mean(feats, axis=1), 1)) / np.expand_dims(np.std(feats, axis=1), 1)

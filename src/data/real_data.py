39168328941dadf119449541170412f08ce7f335
John Zhou <john.ly.zhou@gmail.com>
2022-10-28 14:19:07 -0700

    move real data conversion functions here, bugfix to make nans -1

--- src/data/real_data.py
+++ src/data/real_data.py
@@ -1,3 +1,6 @@
+from typing import List, Tuple
+
+import numpy as np
 from scipy import io
 
 from src.data.environments import DynamicForagingTask, SwitchingStimulusTask
@@ -61,3 +64,41 @@ class SwitchingStimulusData:
         self.performance = data['Performance'].flatten()
         self.left_performance = data['lPerformance'].flatten()
         self.right_performance = data['rPerformance'].flatten()
+
+
+def generate_real_block_params(real_blocks: List[int], real_correct_side: List[int]) -> List[Tuple]:
+    """
+    Generate list of block parameters from real data.
+    :param real_blocks: list of ints, where the index is the trial number and entry is the block number, e.g. [1, 1, 1,
+    2, 2, 2, 2, 3, 3] would represent 3 trials in block 1, 4 in block 2, and 2 in block 3.
+    :param real_correct_side: list of ints, where the index is the trial number and entry is the correct side in the
+    block (note this is not necessarily the rewarded side in nondeterministic environments).
+    :return: list of block parameters, of format (side, reward_probability, num_trials).
+    """
+    real_blocks = np.array(real_blocks)
+    unique, counts = np.unique(real_blocks, return_counts=True)
+    blocks = []
+    if real_correct_side[0] == 1:
+        side = "LEFT"
+    else:
+        side = "RIGHT"
+    for num_trials in counts:
+        blocks.append((side, 1.0, int(num_trials)))
+        if side == "LEFT":
+            side = "RIGHT"
+        else:
+            side = "LEFT"
+    return blocks
+
+
+def convert_real_actions(real_actions: List[int]) -> List[int]:
+    """
+    Convert action data from real experiment to correct format (turn nan nonchoices into incorrect choices).
+    :param real_actions: List of choices made in experiment, in original format where 1: left choice, 2: right choice,
+    nan: no choice.
+    :return: List of converted actions, where 0: left choice or no choice
+    """
+    for i, act in enumerate(real_actions):
+        if np.isnan(act):
+            real_actions[i] = 0
+    return [int(action - 1) for action in real_actions]

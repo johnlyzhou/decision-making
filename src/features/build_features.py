24aaae6de5d3aed5d3f0f24e753246850d85cee6
John Zhou <john.ly.zhou@gmail.com>
2022-10-28 12:15:29 -0700

    refactor, sigmoid fit functions deprecated soon in favor of logistic regression

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -7,8 +7,8 @@ from numpy import ndarray
 def build_ssm_observations(*args: list) -> ndarray:
     """
     Takes in lists of observations and reshapes them into the correct observation format for the SSM library.
-    :param args: lists of observation values of length n (corresponding to trials, blocks, etc.).
-    :return: list of tuples with each type of observation of length n.
+    :param args: Lists of observation values of length n (corresponding to trials, blocks, etc.).
+    :return: List of tuples with each type of observation of length n.
     """
     if len(set([len(arg) for arg in args])) != 1:
         print([len(arg) for arg in args])
@@ -19,7 +19,7 @@ def build_ssm_observations(*args: list) -> ndarray:
 
 
 def compute_foraging_efficiency(actions: Union[List[int], ndarray], rewards: Union[List[int], ndarray]) -> float:
-    """Compute percentage of trials correct across an entire experiment."""
+    """Compute percentage of trials that are correct."""
     if type(actions) is list:
         actions = np.array(actions)
     if type(rewards) is list:
@@ -30,8 +30,8 @@ def compute_foraging_efficiency(actions: Union[List[int], ndarray], rewards: Uni
 def sigmoid_initial_guess(y: List[float]) -> ndarray:
     """
     Guess initial parameters for sigmoid fitting.
-    :param y: trial-averaged decision values for each timestep of the trial
-    :return: set of initial parameters
+    :param y: Trial-averaged decision values for each timestep of the trial.
+    :return: Set of initial parameters.
     """
     s = 14
     for i in range(len(y) - 1):
@@ -43,7 +43,7 @@ def sigmoid_initial_guess(y: List[float]) -> ndarray:
     return np.array([eps, a, s])
 
 
-def driver_func(params, X, y) -> ndarray:
+def sigmoid_mse(params, X, y) -> ndarray:
     """
     Sigmoid curve with additional epsilon "lapse" parameter.
     """
@@ -55,10 +55,10 @@ def driver_func(params, X, y) -> ndarray:
 def sigmoid(X: ndarray, eps: float, alpha: float, s: int) -> ndarray:
     """
     Sigmoid curve with additional epsilon "lapse" parameter.
-    :param eps: float in range [0, 1], epsilon exploration parameter, influences maximum value.
-    :param alpha: logistic growth rate indicating steepness of the curve.
-    :param X: int indicating trial index, where the first trial after the block switch has index 0.
-    :param s: trial index at curve's midpoint.
-    :return: float in range [0, 1] indicating some percentage.
+    :param eps: Float in range [0, 1], epsilon exploration parameter, influences maximum value.
+    :param alpha: Logistic growth rate indicating steepness of the curve.
+    :param X: Int indicating trial index, where the first trial after the block switch has index 0.
+    :param s: Trial index at curve's midpoint.
+    :return: Float in range [0, 1] indicating some percentage.
     """
     return np.float64(eps + (1 - 2 * eps) / (1 + np.exp(-alpha * (X - s))))

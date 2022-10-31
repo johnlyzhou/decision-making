8d5424afb81ab6a5affad0a043d29d33cf388152
John Zhou <john.ly.zhou@gmail.com>
2022-10-31 16:36:44 -0700

    move losses to new file, add nll

---
+++ src/features/losses.py
@@ -0,0 +1,27 @@
+from typing import Callable
+
+import numpy as np
+from numpy import ndarray
+
+from src.features.build_features import sigmoid, binary_logistic
+
+
+def mse_loss(params: list, func: Callable, X: ndarray, y:ndarray) -> ndarray:
+    """
+    Sigmoid curve with additional epsilon "lapse" parameter.
+    """
+    yhat = sigmoid(X, *params)
+    loss = np.sum((yhat - y) ** 2)
+    return loss
+
+
+def binary_nll(params: list, X: ndarray, y: ndarray) -> ndarray:
+    """
+    Calculate negative log likelihood for binary logistic regression.
+    :param params: List of params (b0 and b1) for binary logistic function.
+    :param X: Trial indices from block switch.
+    :param y: Binary target values in {0, 1}.
+    :return: Negative log-likelihood of y given parameters b0 and b1.
+    """
+    p_hat = binary_logistic(X, *params)
+    return np.sum(np.multiply(-y, np.log(p_hat)) - np.multiply((1 - y), (np.log(1 - p_hat))))

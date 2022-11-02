759cc4681d0f36473369dbbea69b25d25b12e6f7
John Zhou <john.ly.zhou@gmail.com>
2022-11-02 16:18:16 -0700

    full pipeline added

--- run.py
+++ run.py
@@ -1,26 +1,52 @@
+import numpy as np
+
 from src.data.generate_synth_data import run_experiment_batch
 from src.data.agents import QLearningAgent, InferenceAgent
 from src.data.environments import DynamicForagingTask
-from src.features.fit_curves import epsilon_sigmoid, get_sigmoid_feats, binary_logistic, get_logistic_feats
+from src.features.fit_curves import get_sigmoid_feats
+from src.features.build_features import compute_foraging_efficiency
 from src.features.losses import mse_loss, binary_nll
-from src.visualization.plot_replications import plot_fitted_block
 
 if __name__ == "__main__":
     data_dir = "/Users/johnzhou/research/decision-making/data"
-    block_choices, labels = run_experiment_batch(DynamicForagingTask, QLearningAgent, num_blocks=10, save=True)
+
+    # Q Learning agent
+    block_choices, labels = run_experiment_batch(DynamicForagingTask, QLearningAgent, num_blocks=100, save=True)
+    np.save(f"{data_dir}/interim/ql_trials.npy", block_choices)
     print(block_choices.shape)
+    E = compute_foraging_efficiency(block_choices,
+                                    save_path=f"{data_dir}/processed/ql_eff.npy")
+    print(E.shape)
     nll_sigmoid_feats = get_sigmoid_feats(block_choices, binary_nll, plot=False, save=True,
-                                          save_path=f"{data_dir}/synth/ql_nll_sig.npy")
+                                          save_path=f"{data_dir}/processed/ql_nll_sig.npy")
     print(nll_sigmoid_feats.shape)
     mse_sigmoid_feats = get_sigmoid_feats(block_choices, mse_loss, plot=False, save=True,
-                                          save_path=f"{data_dir}/synth/ql_mse_sig.npy")
+                                          save_path=f"{data_dir}/processed/ql_mse_sig.npy")
     print(mse_sigmoid_feats.shape)
 
-    block_choices, labels = run_experiment_batch(DynamicForagingTask, InferenceAgent, num_blocks=10, save=True)
+    # Inference agent
+    block_choices, labels = run_experiment_batch(DynamicForagingTask, InferenceAgent, num_blocks=100, save=True)
+    np.save(f"{data_dir}/interim/inf_trials.npy", block_choices)
     print(block_choices.shape)
+    E = compute_foraging_efficiency(block_choices,
+                                    save_path=f"{data_dir}/processed/inf_eff.npy")
+    print(E.shape)
     nll_sigmoid_feats = get_sigmoid_feats(block_choices, binary_nll, plot=False, save=True,
-                                          save_path=f"{data_dir}/synth/inf_nll_sig.npy")
+                                          save_path=f"{data_dir}/processed/inf_nll_sig.npy")
     print(nll_sigmoid_feats.shape)
     mse_sigmoid_feats = get_sigmoid_feats(block_choices, mse_loss, plot=False, save=True,
-                                          save_path=f"{data_dir}/synth/inf_mse_sig.npy")
+                                          save_path=f"{data_dir}/processed/inf_mse_sig.npy")
+    print(mse_sigmoid_feats.shape)
+
+    # Real agent
+    block_choices = np.load(f"{data_dir}/interim/real_trials.npy")
+    print(block_choices.shape)
+    E = compute_foraging_efficiency(block_choices,
+                                    save_path=f"{data_dir}/processed/real_eff.npy")
+    print(E.shape)
+    nll_sigmoid_feats = get_sigmoid_feats(block_choices, binary_nll, plot=False, save=True,
+                                          save_path=f"{data_dir}/processed/real_nll_sig.npy")
+    print(nll_sigmoid_feats.shape)
+    mse_sigmoid_feats = get_sigmoid_feats(block_choices, mse_loss, plot=False, save=True,
+                                          save_path=f"{data_dir}/processed/real_mse_sig.npy")
     print(mse_sigmoid_feats.shape)

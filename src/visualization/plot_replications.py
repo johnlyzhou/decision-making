abdf0c3d92b5964c64adcd632af675a342bdd94e
John Zhou <john.ly.zhou@gmail.com>
2022-11-02 10:23:06 -0700

    update docstrings, add only functions to replicate blockHMM plots

--- src/visualization/plot_replications.py
+++ src/visualization/plot_replications.py
@@ -1,17 +1,35 @@
-from typing import Tuple
+from typing import Tuple, Callable
 
-import numpy as np
-import scipy
 from matplotlib import pyplot as plt
+import numpy as np
 from numpy import ndarray
 
 from src.data.experiments import SynthExperiment
-from src.data.generate_synth_data import INITIAL_GUESS, SIGMOID_PARAM_BOUNDS
-from src.features.fit_curves import epsilon_sigmoid
-from src.features.losses import mse_loss
-from src.utils import build_config, blockify, normalize_choice_block_side, average_choice_blocks, pad_ragged_blocks
+from src.utils import build_config, blockify, normalize_choice_block_side, pad_ragged_blocks
 
 TITLES = ["Lapse", "Slope", "Offset", "Efficiency"]
+
+
+def plot_fitted_block(choice_block: ndarray,
+                      curve_func: Callable,
+                      params: Tuple) -> None:
+    """
+    Plot a block of choices (can be either individual or averaged), and a fitted curve on top.
+    :param choice_block: Single block of binary trial choices.
+    :param curve_func: Curve function, e.g. sigmoid with epsilon parameter or logistic.
+    :param params: Parameters for curve function.
+    :return: None
+    """
+    x_bounds = (0, choice_block.size)
+
+    plt.plot(np.linspace(*x_bounds, num=1000),
+             curve_func(np.linspace(*x_bounds, num=1000), *params),
+             label=f"Parameters: {params}")
+    plt.scatter(range(choice_block.size), list(choice_block))
+    plt.xlim(x_bounds)
+    plt.ylim([0, 1])
+    plt.legend()
+    plt.show()
 
 
 def plot_heat_map(feats: ndarray,
@@ -27,16 +45,17 @@ def plot_heat_map(feats: ndarray,
     Plots heatmaps from Le et al. 2022, where the axes are parameter settings of the agents and the heat represents the
     magnitude of each fitted feature of the sigmoid curve + foraging efficiency from an average over multiple blocks
     of simualted trials.
-    :param feats:
-    :param feat_idx:
-    :param xrange:
-    :param yrange:
-    :param xnum:
-    :param ynum:
-    :param xlabel:
-    :param ylabel:
-    :param transpose:
-    :return:
+    :param feats: Parameters from fitting sigmoid curves to averaged blocks of trials.
+    :param feat_idx: Index of feature to be heatmapped (one of TITLES).
+    :param xrange: Range of values for x, a parameter of the agent (typically epsilon or Prew).
+    :param yrange: Range of values for y, a parameter of the agent (typically learning rate or Pswitch).
+    :param xnum: Number of evenly spaced x values within xrange.
+    :param ynum: Number of evenly spaced y values within yrange.
+    :param xlabel: Label of x parameter for plotting.
+    :param ylabel: Label of y parameter for plotting.
+    :param transpose: Whether to transpose feature matrix after reshaping (useful when you've switched epsilon and
+    learning rate or Prew and Pswitch in the order of inner and outer loops when generating data.
+    :return: None
     """
     fig, ax = plt.subplots()
     feat = feats[feat_idx, :]
@@ -56,55 +75,6 @@ def plot_heat_map(feats: ndarray,
     ax.set_title(TITLES[feat_idx])
     ax.axis([*xrange, *yrange])
     fig.colorbar(c, ax=ax)
-
-
-def plot_block_sigmoid(eps: float = 0.1,
-                       lr: float = 0.1,
-                       pswitch: float = None,
-                       prew: float = None,
-                       trial_bounds: Tuple[int, int] = (15, 25),
-                       true_pr_rew: float = 1.0,
-                       num_blocks: int = 100) -> None:
-    """
-    Fit and plot a sigmoid curve to the average choices of num_blocks trials of an agent.
-    :param eps: Epsilon parameter for Q-learning agent.
-    :param lr: Learning rate parameter for Q-learning agent.
-    :param pswitch: P_switch parameter for inference agent.
-    :param prew: P_reward parameter for inference agent.
-    :param trial_bounds: Range of num_trials in the block.
-    :param true_pr_rew: Probability that the environment rewards the correct choice.
-    :param num_blocks: Number of blocks to simulate and average across for fitting.
-    :return: None
-    """
-    if eps and lr:
-        config = build_config("DynamicForagingTask", "QLearningAgent", num_blocks, trial_bounds, true_pr_rew, eps=eps,
-                              lr=lr)
-    elif pswitch and prew:
-        config = build_config("DynamicForagingTask", "InferenceAgent", num_blocks, trial_bounds, true_pr_rew, pr_switch=pswitch,
-                              pr_rew=prew)
-    else:
-        raise ValueError("Need to specify parameters for QLearningAgent OR InferenceAgent!")
-
-    expt = SynthExperiment(config)
-    expt.run()
-    actions = expt.agent.action_history
-    blocked_actions = blockify(expt.blocks, actions)
-    blocks = expt.blocks
-    normalized_actions = [normalize_choice_block_side(blocked_actions[block_idx], blocks[block_idx][0])
-                          for block_idx in range(len(blocks))]
-    max_len = max([block[2] for block in blocks])
-    averaged_blocks = average_choice_blocks(normalized_actions, max_len=max_len, mode="truncate")
-
-    x_obs = range(len(averaged_blocks))
-    y_obs = averaged_blocks
-    params = scipy.optimize.minimize(mse_loss, INITIAL_GUESS, method="Nelder-Mead", args=(x_obs, y_obs),
-                                     bounds=SIGMOID_PARAM_BOUNDS).x
-    plt.plot(np.linspace(0, len(averaged_blocks), num=1000),
-             epsilon_sigmoid(np.linspace(0, len(averaged_blocks), num=1000), *params), 'r-',
-             label='fit: eps=%5.3f, k=%5.3f, x0=%5.3f' % tuple(params))
-    plt.legend()
-    plt.scatter(x_obs, averaged_blocks)
-    plt.show()
 
 
 def plot_behavior_simulation(eps: float = 0.1,

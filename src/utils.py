233c135de8ecace65ff7f7b9ea030fe2adfc5cba
John Zhou <john.ly.zhou@gmail.com>
2022-11-11 09:47:31 -0800

    move feature methods to different file

--- src/utils.py
+++ src/utils.py
@@ -151,25 +151,3 @@ def average_choice_blocks(normalized_choice_blocks: List[List[int]],
         return ragged_sum / idx_count
     else:
         raise NotImplementedError
-
-
-def normalize_features(feats: ndarray) -> ndarray:
-    """
-    Normalize features for clustering/classification.
-    :param feats: features to normalize, should be of shape (num_samples, num_features).
-    :return: standardized features (subtract mean and divide by standard devation across samples for each feature).
-    """
-    return (feats - np.expand_dims(np.mean(feats, axis=1), 1)) / np.expand_dims(np.std(feats, axis=1), 1)
-
-
-def remove_invalid_fits(feats: ndarray) -> ndarray:
-    """
-    Remove samples from an array of sigmoid features that fall outside of predetermined bounds.
-    :param feats: Array of sigmoid features.
-    :return: Array of sigmoid features with invalid samples removed.
-    """
-    # invalid_alpha = feats[:, 1] < 0
-    invalid_low_s = feats[:, 2] < 0
-    invalid_high_s = feats[:, 2] > 14
-    invalid_idxs = np.argwhere(invalid_low_s | invalid_high_s)
-    return np.delete(feats, invalid_idxs, axis=0)

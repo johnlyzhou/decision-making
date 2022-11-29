b47b48eb5b4d81e07dd945baffb4e1e9b00557e3
John Zhou <john.ly.zhou@gmail.com>
2022-11-29 10:51:26 -0800

    setup data dir structure

--- setup.py
+++ setup.py
@@ -1,3 +1,6 @@
+import os
+from pathlib import Path
+
 from setuptools import find_packages, setup
 
 
@@ -5,3 +8,12 @@ setup(
     name='src',
     packages=find_packages(),
 )
+
+# Set up directories
+repo_dir = os.path.dirname(os.path.realpath(__file__))
+
+dir_names = ["data/processed", "data/interim", "data/raw"]
+
+for dir_name in dir_names:
+    dir_path = Path(f"{repo_dir}/{dir_name}")
+    dir_path.mkdir(parents=True, exist_ok=True)
